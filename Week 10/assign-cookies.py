5239b367b16bbd638fdbd0470564afdd6e7f0c61
ZiadAladawySE <zeyad20225162fcihelwanedueg@gmail.com>
2025-02-07 07:17:46 +0200

    finished this one

--- Week 10/assign-cookies.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def findContentChildren(self, g: list[int], s: list[int]) -> int:
-        it = iter(sorted(s))
-        return sum(any(gi <= si for si in it) for gi in sorted(g))
--- Week 10/count-pairs-whose-sum-is-less-than-target.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def countPairs(self, nums: List[int], target: int) -> int:
-
-        return sum(map(lambda x: x[0]+x[1] < target,
-                       combinations(nums, 2)))
